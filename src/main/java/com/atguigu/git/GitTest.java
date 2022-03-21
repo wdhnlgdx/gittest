cda1bd8e96d09392f760fe0654503669c6ebdf14
wd <wd@163.com>
2022-03-21 17:53:57 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;/**
+ * @description:
+ * @author: wd
+ * @create: 2022-03-21 17:51
+ */
+
+/**
+ @description:
+ @author: wd
+ @create: 2022-03-21 17:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
